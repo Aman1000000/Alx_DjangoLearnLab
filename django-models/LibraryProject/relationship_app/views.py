0220391527414ba7cce4a69d7fb14b8621949445
Aman1000000 <yimeramanuel2@gmail.com>
2025-08-03 07:49:15 -0700

    b

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from relationship_app.models import Book
 from django.views.generic import DetailView  
 from relationship_app.models import Library
+from .models import Library  
 
 def list_books(request):
     books = Book.objects.all()
@@ -9,5 +10,10 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'  
+    context_object_name = 'library' 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)  # Filter books by the library
+        return context
