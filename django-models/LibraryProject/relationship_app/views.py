f48a093f7d8c037b5b47a77531aadaae5d8758a5
Aman1000000 <yimeramanuel2@gmail.com>
2025-08-03 08:01:46 -0700

    r

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,11 +9,10 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
     
 class LibraryDetailView(DetailView):
-    model = Library  # Specify the model for this view
-    template_name = 'relationship_app/library_detail.html'  # Path to the template
-    context_object_name = 'library'  # Name of the context variable to use in the template
-
+    model = Library  #
+    template_name = 'relationship_app/library_detail.html'  
+    context_object_name = 'library' 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # Call the base implementation first
-        context['books'] = Book.objects.filter(library=self.object)  # Fetch books related to the library
+        context = super().get_context_data(**kwargs)  
+        context['books'] = Book.objects.filter(library=self.object)  
         return context
